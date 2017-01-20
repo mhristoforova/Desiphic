e4bf53d17a62ce007d98ec91f66086e2e03eed03
mhristoforova <mhristoforova@gmail.com>
2017-01-20 10:06:58 +0000

    Final Fixes

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,26 +1,18 @@
 $(document).ready(function() {
 
     /*Call pagepiling plugin */
-        $('#pagepiling').pagepiling({
-            loopTop: true,
-            loopBottom: false,
-            keyboardScrolling: true,
-            verticalCentered: false,
-            navigation: {
-                'bulletsColor':false,
-                'position': 'right',
-                'tooltips': false
-            },
-        });
 
-    //Hide pagepiling if mobile
-
-    $(window).resize(function () {
-        let viewportWidth = Number($(window).width());
-        if (viewportWidth < 500) {
-            $("div#pagepiling").removeAttr('pagepiling');
-        }
-    });
+            $('#pagepiling').pagepiling({
+                loopTop: true,
+                loopBottom: false,
+                keyboardScrolling: true,
+                verticalCentered: false,
+                navigation: {
+                    'bulletsColor':false,
+                    'position': 'right',
+                    'tooltips': false
+                },
+            });
 
     /*$("#status").fadeOut("slow");
     $("#preloader").delay(500).fadeOut("slow").remove();*/
